bc08e0977091474152d78b127fbb73cb80ea6bf1
Seth Ketsi <christambassador12@gmail.com>
2024-01-15 23:48:23 +0000

    updated

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <form className="login">
+        <h1>Login</h1>
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button>Login</button>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Register = () => {
   return (
     <form className="register">
+        <h1>Register</h1>
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button>Register</button>
