01ecfa5f43728f268ff5ed45c386f5e519322bfc
Seth Ketsi <christambassador12@gmail.com>
2024-01-18 20:56:17 +0000

    login working

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const { mongoose } = require('mongoose');
-const User = require('./models/User')
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = '3YHeXFSTUD0DepVCsdscsd2435wdaDWSDAWassd';
 
 app.use(cors());
 app.use(express.json());
@@ -12,11 +17,31 @@ mongoose.connect('mongodb+srv://blog:3YHeXFSTUD0DepVC@cluster0.1h0enes.mongodb.n
 app.post('/register', async (req, res) => {
     const {Username, Password} = req.body;
     try{
-        const userDoc = await User.create({Username, Password});
+        const userDoc = await User.create({
+            Username,
+            Password: bcrypt.hashSync(Password, salt),
+        });
         res.json(userDoc);
     } catch(e) {
         // return res.status(400).json({ error: 'Username already exist' });
         res.status(400).json(e);
+    }
+
+});
+
+app.post('/login', async (req, res) => {
+    const {Username, Password} = req.body;
+    const userDoc = await User.findOne({Username});
+    const passOk = bcrypt.compareSync(Password, userDoc.Password);
+    if (passOk) {
+        jwt.sign({Username,id:userDoc._id}, secret, {}, (err, token) => {
+           if (err) {
+            res.cookie('token', token).json({Ok});
+           }
+        });
+        // res.json(userDoc);
+    } else {
+        res.status(400).json({error: 'Wrong Credentials'});
     }
 
 });
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+  const [Username, setUsername] = useState('');
+  const [Password, setPassword] = useState('')
+
+  async function login(ev) {
+    ev.preventDefault();
+
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({Username, Password}),
+      headers: {'Content-Type':'application/json'},
+    });
+
+    if (response.status === 200) {
+      alert('Login successful!');
+    } else {
+      alert('Login failed!');
+    }
+  }
+
   return (
-    <form className="login">
+    <form className="login" onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Username" value={Username} onChange={ev => setUsername(ev.target.value)}/>
+        <input type="password" placeholder="Password" value={Password} onChange={ev => setPassword(ev.target.value)}/>
         <button>Login</button>
     </form>
   )
