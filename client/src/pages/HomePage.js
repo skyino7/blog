008700f124db10e4960a9065ae1d6e81328f42f6
Seth Ketsi <christambassador12@gmail.com>
2024-01-29 23:49:08 +0000

    Displaying post from database

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from '../post'
 
 const Homepage = () => {
+
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     fetch('http://localhost:4000/post').then(response => {
       response.json().then(posts => {
-        console.log(posts);
+        setPosts(posts);
       });
     });
   }, []);
   return (
     <>
-        <Post />
-        <Post />
-        <Post />
+       {posts.length > 0 && posts.map(post => (
+          <Post {...post} />
+       ))}
     </>
   )
 }
--- client/src/post.js
+++ client/src/post.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({title, summary, file, content, createdAt}) => {
   return (
     <div className="post">
         <div className="image">
             <img src="https://phoenixnap.com/kb/wp-content/uploads/2023/12/how-to-ssh-into-a-docker-container.png" alt="Blog Post" />
         </div>
         <div className="texts">
-            <h2>How to SSH into a Docker Container</h2>
+            <h2>{title}</h2>
         <p className="info">
             <a className="author">John Doe</a>
-            <time className="time">May 14, 2021 10:53</time>
+            <time className="time">{createdAt}</time>
         </p>
-        <p className="summary">The primary purpose of the SSH protocol is to enable a secure network connection to a remote server. Although Docker containers do not run full-fledged operating systems, they all have private IP addresses, so it is possible to use SSH to establish a local connection to their shell.</p>
+        <p className="summary">{summary}</p>
         </div>
     </div>
   )
