ec6c390d9e94e3ea6bde4c1f5c8b0cd48c988df6
Seth Ketsi <christambassador12@gmail.com>
2024-01-31 22:59:23 +0000

    updated

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+// import {response} from 'express';
 
 const PostPage = () => {
+    const {id} = useParams();
+    useEffect(() => {
+        fetch(`http://localhost:4000/post/${id}`).then(response => {
+            response.json().then(data => {
+                console.log(data);
+            })
+        });
+    }, [id]);
   return (
     <div>PostPage</div>
-  )
+  );
 }
 
 export default PostPage
