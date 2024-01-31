9a665690841fd91130e5803fe879b23652141956
Seth Ketsi <christambassador12@gmail.com>
2024-01-31 23:25:31 +0000

    updated

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import {response} from 'express';
 import { useState } from 'react';
 
 const PostPage = () => {
