40bcb96cd3802a872e4435ddb37ede228198e594
Seth Ketsi <christambassador12@gmail.com>
2024-01-31 22:28:29 +0000

    updated

--- client/src/post.js
+++ client/src/post.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { format } from 'date-fns'
 import { Link } from 'react-router-dom'
 
-const Post = ({title, summary, file, content, createdAt, author}) => {
+const Post = ({_id, title, summary, file, content, createdAt, author}) => {
 
   return (
     <div className="post">
         <div className="image">
-          <Link to={'/post/id'}>
+          <Link to={`/post/${_id}`}>
             <img src={'http://localhost:4000/'+file} alt="Blog Post" />
           </Link>
         </div>
         <div className="texts">
-          <Link to={'/post/id'}>
+          <Link to={`/post/${_id}`}>
             <h2>{title}</h2>
           </Link>
         <p className="info">
