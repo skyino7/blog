20721f827aed7e2c8548f3b98ea57cb27786eca8
Seth Ketsi <christambassador12@gmail.com>
2024-01-31 22:33:48 +0000

    Post page created to display content

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import { UserContextProvider } from './UserContext';
 import CreatePost from './pages/CreatePost';
+import PostPage from './pages/PostPage';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/create" element={<CreatePost  />} />
+          <Route path="/post/:id" element={<PostPage  />} />
         </Route>
       </Routes>
     </UserContextProvider>
