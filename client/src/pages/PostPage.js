885c38c9d718e4498df319d3d7d9a32fbee335f0
Seth Ketsi <christambassador12@gmail.com>
2024-02-01 23:08:18 +0000

    Edit Post Page Created

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import { format } from 'date-fns';
 import {UserContext} from '../UserContext';
+import { Link } from 'react-router-dom';
 
 const PostPage = () => {
     const [postInfo, setPostInfo] = useState(null);
@@ -25,8 +26,13 @@ const PostPage = () => {
       <p>By {postInfo.author.Username} on {format((postInfo.createdAt), 'MMM d, yyyy, HH:mm')}</p>
       {userInfo.id === postInfo.author._id && (
         <div className='actions'>
-          <a className='edit' href={`/post/${postInfo.id}/edit`}>Edit Post</a>
-          <a className='delete' href={`/post/${postInfo.id}/delete`}>Delete Post</a>
+          <Link className='edit' to={`/edit/${postInfo._id}`}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+            </svg>
+            Edit Post
+          </Link>
+          <Link className='delete' to={`/post/${postInfo._id}`}>Delete Post</Link>
         </div>
       )}
       <div className='image'>
